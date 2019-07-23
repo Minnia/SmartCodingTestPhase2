fdef53e7e7f33a0ec8ab0ea24c6cb8603152a3e5
Jasmine Pishnemazi <Jasmine@Tylers-MacBook-Pro-2.local>
2019-07-23 13:29:49 +0200

    initial commit

---
+++ PythonTest/test.py
@@ -0,0 +1,19 @@
+def mult(number):
+   mult = number * number
+   return mult
+print(3*3)
+def add(a,b):
+    add = a + b
+    return add
+print(1+3)
+#Data structure
+numbers = [1,2,3,6]
+def sumOfListNumbers(someList):
+    index = 0
+    for num in numbers:
+        index+=num
+    return index
+print(sumOfListNumbers((1,2,3,6)))
+assert sumOfListNumbers(numbers) == 12
+assert mult(3) == 9
+assert add(1,3) == 4
